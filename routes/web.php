7facb66a5503b91c5c5a4d25d1d452f16aa0da6d
api_laravel-at-978029349661 <api_laravel-at-978029349661>
2020-05-17 13:18:38 -0300

    Removendo necessidade de login

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@universidades')->name('universidades');
+Route::get('/', 'HomeController@universidades')->name('universidades');
 Route::get('/hospitais', 'HomeController@hospitais')->name('hospitais');
 Route::get('/materiais', 'HomeController@materiais')->name('hospitais');
